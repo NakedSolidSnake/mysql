95eddb674f4045cc516e36d372181766b6e1c017
solidcris <cssouza@gmail.com>
2023-03-07 07:33:09 -0300

    insert

--- src/main.c
+++ src/main.c
@@ -6,6 +6,9 @@ int main (int argc, char *argv[])
 
 	MYSQL mysql;
 
+	char buffer [1024] = {0};
+	char *query = "insert into person_tb (name, age) values ('%s', %d);";
+
 	mysql_init(&mysql);
 
 	if (mysql_real_connect(&mysql,"db","root","root","person_db",0,NULL,0) != NULL)
@@ -16,6 +19,10 @@ int main (int argc, char *argv[])
 	{
 		fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&mysql));
 	}
+
+	snprintf (buffer, 1024, query, "Nova pessoa", 25);
+
+	mysql_query(&mysql, buffer);
 
 	mysql_close(&mysql);
 
