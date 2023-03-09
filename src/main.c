7d4be9daf3f247987c6c1bdb5dbc4a9f583c47a6
solidcris <cssouza@gmail.com>
2023-03-09 08:19:14 -0300

    select

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@ int main (int argc, char *argv[])
 {
 
 	MYSQL mysql;
+	MYSQL_RES *result_set;
+	MYSQL_ROW row;
+	unsigned int num_fields;
+	unsigned int i;
 
 	char buffer [1024] = {0};
 	// char *query = "insert into person_tb (name, age) values ('%s', %d);";
 
 	// char *query = "UPDATE person_tb SET name = '%s' WHERE id = %d;";
 
-	char *query = "DELETE FROM person_tb WHERE id = %d;";
+	// char *query = "DELETE FROM person_tb WHERE id = %d;";
+
+	char *query = "SELECT * FROM person_tb;";
 
 	mysql_init(&mysql);
 
@@ -27,6 +33,24 @@ int main (int argc, char *argv[])
 	snprintf (buffer, 1024, query, 1);
 
 	mysql_query(&mysql, buffer);
+
+	result_set = mysql_store_result(&mysql);
+    if (result_set)  // there are rows
+    {
+        num_fields = mysql_num_fields(result_set);
+        while ((row = mysql_fetch_row(result_set)))
+		{
+			unsigned long *lengths;
+			lengths = mysql_fetch_lengths(result_set);
+			for(i = 0; i < num_fields; i++)
+			{
+				printf("[%.*s] ", (int) lengths[i],	row[i] ? row[i] : "NULL");
+			}
+			printf("\n");
+		}
+
+		mysql_free_result(result_set);
+    }
 
 	mysql_close(&mysql);
 
